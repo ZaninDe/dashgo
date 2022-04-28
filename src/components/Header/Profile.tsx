32d6ceb5cc05ffe9ca5a79ce31f9214449bb7ac9
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-04-28 17:40:17 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({showProfileData = true}: ProfileProps) {
   return (
     <Flex align="center">
-         <Box mr="4" textAlign="right">
-           <Text> Gabriel Zanin</Text>
-           <Text color="gray.300" fontSize="small">
-             gabriel.zaninde@gmail.com
-           </Text>
-         </Box>
+         {
+           showProfileData && (
+            <Box mr="4" textAlign="right">
+              <Text> Gabriel Zanin</Text>
+              <Text color="gray.300" fontSize="small">
+                gabriel.zaninde@gmail.com
+              </Text>
+            </Box>)
+         }
 
          <Avatar size="md" name="Gabriel Zanin" src="https://github.com/ZaninDe.png"/>
        </Flex>
