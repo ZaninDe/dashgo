29c6e50eea92b463e74f5334ca6f6a26d7942259
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-05-02 19:15:38 -0300

    validate forms with react-hook-forms and yup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
-import { Button, Flex, Stack } from '@chakra-ui/react'
-import { Input } from '../components/Form/Input'
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm, } from 'react-hook-form';
+import * as yup from 'yup';
+import { Input } from '../components/Form/Input';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required().email(),
+  password: yup.string().required(),
+})
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+  const { errors } = formState
+
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (value) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(value)
+  }
+
   return (
    <Flex
      w="100vw"
@@ -16,24 +41,25 @@ export default function SignIn() {
        bg="gray.800"
        p="8"
        borderRadius={8}
-       flexDir="column"
+        flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
      >
        <Stack spacing="4">
-        <Input name="email" type="email" label="E-mail"></Input>
-        <Input name="password" type="password" label="Senha"></Input>
+        <Input name="email" type="email" label="E-mail" error={errors.email} {...register('email')}></Input>
+          <Input name="password" type="password" label="Senha" error={errors.password} {...register('password')}></Input>
        </Stack>
 
        <Button
          type="submit"
          mt="6"
          colorScheme="pink"
-         size="lg"
-         placeholder="Password"
+          size="lg"
+          isLoading={formState.isSubmitting}
        >
          Entrar
        </Button>
      </Flex>
    </Flex>
-   
+
   )
 }
