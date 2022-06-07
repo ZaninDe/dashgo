50624f72367fd2661ec67bdbc06ac024d4e483a9
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-06-07 14:44:05 -0300

    react query config

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,110 +1,146 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+  useBreakpointValue,
+  Spinner,
+} from '@chakra-ui/react';
 import Link from 'next/link';
 import { useEffect } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { useQuery } from 'react-query';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+
+    return data;
+  });
+
+  console.log(data)
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
-  })
+  });
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
   return (
     <Box>
       <Header />
 
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-          <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Users</Heading>
+        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+          <Flex mb='8' justify='space-between' align='center'>
+            <Heading size='lg' fontWeight='normal'>
+              Users
+            </Heading>
 
-            <Link href="/users/create" passHref>
+            <Link href='/users/create' passHref>
               <Button
-                as="a"
-                size="sm"
-                fontSize="sm"
-                colorScheme="pink"
-                leftIcon={<Icon
-                  as={RiAddLine} fontSize="20"
-                />}
+                as='a'
+                size='sm'
+                fontSize='sm'
+                colorScheme='pink'
+                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
               >
                 Create a new
               </Button>
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>User</Th>
-                {isWideVersion && <Th>Creation Date</Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gabriel Zanin</Text>
-                    <Text fontSize="sm" color="gray.300">gabriel.zaninde@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>27 de Abril, 2022</Td>}
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Error fetching data</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme='whiteAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                      <Checkbox colorScheme='pink' />
+                    </Th>
+                    <Th>User</Th>
+                    {isWideVersion && <Th>Creation Date</Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme='pink' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>Gabriel Zanin</Text>
+                        <Text fontSize='sm' color='gray.300'>
+                          gabriel.zaninde@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>27 de Abril, 2022</Td>}
+                  </Tr>
+                </Tbody>
 
-              </Tr>
-            </Tbody>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme='pink' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>Gabriel Zanin</Text>
+                        <Text fontSize='sm' color='gray.300'>
+                          gabriel.zaninde@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>27 de Abril, 2022</Td>}
+                  </Tr>
+                </Tbody>
 
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gabriel Zanin</Text>
-                    <Text fontSize="sm" color="gray.300">gabriel.zaninde@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>27 de Abril, 2022</Td>}
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme='pink' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>Gabriel Zanin</Text>
+                        <Text fontSize='sm' color='gray.300'>
+                          gabriel.zaninde@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>27 de Abril, 2022</Td>}
+                  </Tr>
+                </Tbody>
+              </Table>
 
-              </Tr>
-            </Tbody>
-
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gabriel Zanin</Text>
-                    <Text fontSize="sm" color="gray.300">gabriel.zaninde@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>27 de Abril, 2022</Td>}
-
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
-  )
+  );
 }
