eb0b436ec07d7d468d048569d857d1c51a937fec
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-06-09 19:02:28 -0300

    sign in button updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,38 +28,38 @@ export default function SignIn() {
   }
 
   return (
-   <Flex
-     w="100vw"
-     h="100vh"
-     align="center"
-     justify="center"
-   >
-     <Flex
-       as="form"
-       width="100%"
-       maxWidth={360}
-       bg="gray.800"
-       p="8"
-       borderRadius={8}
-       flexDir="column"
-       onSubmit={handleSubmit(handleSignIn)}
-     >
-       <Stack spacing="4">
-        <Input name="email" type="email" label="E-mail" error={errors.email} {...register('email')}></Input>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" error={errors.email} {...register('email')}></Input>
           <Input name="password" type="password" label="Senha" error={errors.password} {...register('password')}></Input>
-       </Stack>
+        </Stack>
 
-       <Button
-        type="submit"
-        mt="6"
-        colorScheme="pink"
-        size="lg"
-        isLoading={formState.isSubmitting}
-       >
-         Entrar
-       </Button>
-     </Flex>
-   </Flex>
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
+          Sign in
+        </Button>
+      </Flex>
+    </Flex>
 
   )
 }
